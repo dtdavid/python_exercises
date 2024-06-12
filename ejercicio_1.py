40b222c863b03c36bd79cb8fdf5477eaf3460958
david.docampo <david.docampo@gmail.com>
2024-06-12 12:46:49 +0200

    ejercicio_1

---
+++ ejercicio_1.py
@@ -0,0 +1,5 @@
+'''Escribir una secuencia de instrucciones de python que permita declarar 3 variables a, b, c asignandole respecívamente los valores de 1, 'Francia, 36.2 y luego mostrar los valores de estas variables en la consola'''
+a = 1
+b = 'Francia'
+c = 36.2
+print(f"Los valores de las variables son:\na = {a}\nb = {b}\nc = {c}")
